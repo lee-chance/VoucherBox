657b517fa33ecb6f322bd59f051312b990b8fa5f
Changsu Lee <729mail2@gmail.com>
2022-11-20 20:48:27 +0900

    Initial Commit

---
+++ VoucherBox/VoucherBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoucherBoxApp.swift
+//  VoucherBox
+//
+//  Created by Changsu Lee on 2022/11/20.
+//
+
+import SwiftUI
+
+@main
+struct VoucherBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
